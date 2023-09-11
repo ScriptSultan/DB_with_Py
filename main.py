9048cdaeeda93b8b3c50a87dd2d0abe38ac9c586
Denis Master <mymumhateyou26@mail.ru>
2023-09-11 18:47:50 +0300

    переделал

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def create_db(conn):
                 """)
     return cur.fetchall()
 
+
 def add_client(conn, first_name, last_name, email, phones=None):
     with conn.cursor() as cur:
         cur.execute("""
@@ -38,11 +39,38 @@ def add_phone(conn, client_id, phone):
 
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
     with conn.cursor() as cur:
-        cur.execute("""
-                UPDATE name_bd
-                SET first_name=%s, last_name=%s, email=%s, phones=%s
-                WHERE client_is=%s;
-                """), (first_name, last_name, email, phones, client_id)
+        if first_name is None:
+            pass
+        else:
+            cur.execute("""
+                    UPDATE name_bd
+                    SET first_name=%s
+                    WHERE client_is=%s;
+                    """), (first_name, client_id)
+        if last_name is None:
+            pass
+        else:
+            cur.execute("""
+                    UPDATE name_bd
+                    SET last_name=%s
+                    WHERE client_is=%s;
+                    """), (last_name, client_id)
+        if email is None:
+            pass
+        else:
+            cur.execute("""
+                    UPDATE name_bd
+                    SET email=%s
+                    WHERE client_is=%s;
+                    """), (email, client_id)
+        if phones is None:
+            pass
+        else:
+            cur.execute("""
+                    UPDATE name_bd
+                    SET phones=%s
+                    WHERE client_is=%s;
+                    """), (phones, client_id)
     return cur.fetchone()
 
 
@@ -51,25 +79,52 @@ def delete_phone(conn, client_id):
         cur.execute("""
                 DELETE phone FROM phone_t
                 WHERE client_id=%s
-                """), (client_id)
+                """), (client_id,)
     return cur.fetchone()
 
 
 def delete_client(conn, client_id):
     with conn.cursor() as cur:
         cur.execute("""
+                DELETE FROM phone_t
+                WHERE client_id=%s;"""), (client_id,)
+        cur.execute("""     
                 DELETE FROM name_bd
-                WHERE client_id=%s
-                """), (client_id)
-    return
+                WHERE client_id=%s;
+                """), (client_id,)
+    return "Клиент удален"
 
 
-def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+def find_client(conn, first_name=None, last_name=None, email=None):
     with conn.cursor() as cur:
-        cur.execute("""
-                SELECT first_name, last_name, email, phone FROM name_bd
-                WHERE first_name=%s AND last_name=%s AND email=%s AND phone=%s
-                """), (first_name, last_name, email, phone)
+        if first_name is None and last_name is None:
+            pass
+        else:
+            cur.execute("""
+                    SELECT first_name, last_name, email, phone FROM name_bd
+                    WHERE first_name=%s AND last_name=%s
+                    """), (first_name, last_name)
+        if first_name is None and email is None:
+            pass
+        else:
+            cur.execute("""
+                    SELECT first_name, last_name, email, phone FROM name_bd
+                    WHERE first_name=%s AND email=%s
+                    """), (first_name, email)
+        if last_name is None and email is None:
+            pass
+        else:
+            cur.execute("""
+                    SELECT first_name, last_name, email, phone FROM name_bd
+                    WHERE last_name=%s AND email=%s
+                    """), (last_name, email)
+        if email is None:
+            pass
+        else:
+            cur.execute("""
+                    SELECT first_name, last_name, email, phone FROM name_bd
+                    WHERE email=%s
+                    """), (email,)
     return cur.fetchone()
 
 
